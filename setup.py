608bb46a623f609ea69f72162769258dca9e9d88
Kenneth Reitz <me@kennethreitz.com>
2012-11-23 05:40:49 -0500

    setup.py

---
+++ setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+APP_NAME = 'requests_oauthlib'
+VERSION = '0.2.0'
+
+settings = dict()
+
+
+# Publish Helper.
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+
+settings.update(
+    name=APP_NAME,
+    version=VERSION,
+    description='OAuthlib authentication support for Requests.',
+    long_description=open('README.rst').read(),
+    author='Kenneth Reitz',
+    author_email='me@kennethreitz.com',
+    url='https://github.com/requests/requests-oauthlib',
+    packages= ['requests_oauthlib',],
+    install_requires=['oauthlib'],
+    license='ISC',
+    classifiers=(
+        # 'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        # 'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ),
+)
+
+
+
+setup(**settings)
