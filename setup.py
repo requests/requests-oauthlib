a5a1ef1734b032f082eaa92af9b4b79614f486de
Cory Benfield <lukasaoz@gmail.com>
2013-05-23 19:36:35 +0100

    Update setup.py for downstream packaging.

--- setup.py
+++ setup.py
@@ -41,19 +41,21 @@ settings.update(
     author='Kenneth Reitz',
     author_email='me@kennethreitz.com',
     url='https://github.com/requests/requests-oauthlib',
-    packages= ['requests_oauthlib',],
-    install_requires=['oauthlib>=0.3.7'],
+    packages=['requests_oauthlib'],
+    install_requires=['oauthlib>=0.3.7', 'requests>=1.0.0'],
     license='ISC',
     classifiers=(
-        # 'Development Status :: 5 - Production/Stable',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
-        # 'Programming Language :: Python :: 2.5',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
     ),
+    zip_safe=False,
+    tests_require=['mock'],
+    test_suite='tests'
 )
 
 setup(**settings)
