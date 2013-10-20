6583b47780571c625d01cc220f19b4365a5e8e74
Chunliang Lu <aptenom@gmail.com>
2013-10-20 20:48:04 +0800

    remove the protected_request compliance hook for Sina Weibo. encode the _content of the token response to UTF8 in the access_token_response hook.

--- requests_oauthlib/compliance_fixes/weibo.py
+++ requests_oauthlib/compliance_fixes/weibo.py
@@ -8,17 +8,10 @@ def weibo_compliance_fix(session):
     def _missing_token_type(r):
         token = loads(r.text)
         token['token_type'] = 'Bearer'
-        r._content = dumps(token)
+        r._content = dumps(token).encode('UTF-8')
         return r
-
-    def _non_compliant_param_name(url, headers, data):
-        token = [('access_token', session._client.access_token)]
-        url = add_params_to_uri(url, token)
-        return url, headers, data
 
     session._client.default_token_placement = 'query'
     session.register_compliance_hook('access_token_response',
                                      _missing_token_type)
-    session.register_compliance_hook('protected_request',
-                                     _non_compliant_param_name)
     return session
