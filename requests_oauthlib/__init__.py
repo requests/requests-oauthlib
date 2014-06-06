8eefbc9800769c802836167a4e567ccfae9ac14e
Ib Lundgren <ib.lundgren@gmail.com>
2014-06-06 12:42:24 +0100

    Warn users with an outdated verion of requests. Fix #77.

--- requests_oauthlib/__init__.py
+++ requests_oauthlib/__init__.py
@@ -5,6 +5,12 @@ from .oauth2_session import OAuth2Session, TokenUpdated
 
 __version__ = '0.4.0'
 
+import requests
+if requests.__version__ < '2.0.0':
+    msg = ('You are using requests version %s, which is older than '
+           'requests-oauthlib expects, please upgrade to 2.0.0 or later.')
+    raise Warning(msg % requests.__version__)
+
 import logging
 try:  # Python 2.7+
     from logging import NullHandler
