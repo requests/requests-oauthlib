244628c1c761a08d109f33688cb133c3b3caf418
Dejan SANADER <d.sanader@planet-work.com>
2016-01-12 17:17:12 +0100

    Rename _refresh flag to withhold_token

--- requests_oauthlib/oauth2_session.py
+++ requests_oauthlib/oauth2_session.py
@@ -287,7 +287,7 @@ class OAuth2Session(requests.Session):
             }
 
         r = self.post(token_url, data=dict(urldecode(body)), auth=auth,
-            timeout=timeout, headers=headers, verify=verify, _refresh=True)
+            timeout=timeout, headers=headers, verify=verify, withhold_token=True)
         log.debug('Request to refresh token completed with status %s.',
                   r.status_code)
         log.debug('Response headers were %s and content %s.',
@@ -304,11 +304,11 @@ class OAuth2Session(requests.Session):
             self.token['refresh_token'] = refresh_token
         return self.token
 
-    def request(self, method, url, data=None, headers=None, _refresh=False, **kwargs):
+    def request(self, method, url, data=None, headers=None, withhold_token=False, **kwargs):
         """Intercept all requests and add the OAuth 2 token if present."""
         if not is_secure_transport(url):
             raise InsecureTransportError()
-        if self.token and not _refresh:
+        if self.token and not withhold_token:
             log.debug('Invoking %d protected resource request hooks.',
                       len(self.compliance_hook['protected_request']))
             for hook in self.compliance_hook['protected_request']:
