32e43d7c331883327f814939febbcbfaa7731bb4
Pierre Geier <pierre@neocomy.net>
2017-02-16 15:16:31 +0100

    Plenty compliance fix: cleanup
    More generic approch to convert the Token to snake case.

--- requests_oauthlib/compliance_fixes/plentymarkets.py
+++ requests_oauthlib/compliance_fixes/plentymarkets.py
@@ -1,9 +1,13 @@
 from json import dumps, loads
+import re
 
 from oauthlib.common import to_unicode
 
 
 def plentymarkets_compliance_fix(session):
+
+    def _to_snake_case(n):
+        return re.sub('(.)([A-Z][a-z]+)', r'\1_\2', n).lower()
 
     def _compliance_fix(r):
 
@@ -14,20 +18,8 @@ def plentymarkets_compliance_fix(session):
             return r
 
         fixed_token = {}
-
-        expires = token.pop('expiresIn')
-        if expires is not None:
-            fixed_token['expires_in'] = expires
-
-        access_token = token.pop('accessToken')
-        if access_token is not None:
-            fixed_token['access_token'] = access_token
-
-        refresh_token = token.pop('refreshToken')
-        if refresh_token is not None:
-            fixed_token['refresh_token'] = refresh_token
-
-        fixed_token['token_type'] = 'Bearer'
+        for k, v in token.items():
+            fixed_token[_to_snake_case(k)] = v
 
         r._content = to_unicode(dumps(fixed_token)).encode('UTF-8')
         return r
