7039c5e814328f6fe975f13d6a7a14e0e72cf9c1
Luke Cousins <luke@viovet.co.uk>
2022-01-14 19:03:16 +0000

    Also added support for `access_token_request` hook

--- requests_oauthlib/oauth2_session.py
+++ requests_oauthlib/oauth2_session.py
@@ -338,6 +338,10 @@ class OAuth2Session(requests.Session):
             request_kwargs["params"] = dict(urldecode(body))
         else:
             raise ValueError("The method kwarg must be POST or GET.")
+            
+        for hook in self.compliance_hook["access_token_request"]:
+            log.debug("Invoking access_token_request hook %s.", hook)
+            token_url, headers, request_kwargs = hook(token_url, headers, request_kwargs)
 
         r = self.request(
             method=method,
