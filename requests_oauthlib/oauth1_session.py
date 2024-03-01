416d7382a193d180aac3a2fb7da6f621401ac960
dosisod <39638017+dosisod@users.noreply.github.com>
2024-02-29 20:37:50 -0800

    Remove old `__future__` imports

--- requests_oauthlib/oauth1_session.py
+++ requests_oauthlib/oauth1_session.py
@@ -1,5 +1,3 @@
-from __future__ import unicode_literals
-
 from urllib.parse import urlparse
 
 import logging
--- tests/test_oauth1_session.py
+++ tests/test_oauth1_session.py
@@ -1,4 +1,3 @@
-from __future__ import unicode_literals, print_function
 import unittest
 import requests
 from io import StringIO
