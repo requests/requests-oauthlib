6b8f87c5b7f0f11ba54e4ce29c0228c0b6c2bb2d
jacques troussard <jacques.troussard@gmail.com>
2024-07-18 19:31:04 -0400

    updates unit tests, pending one last test to assert warning message is printed to console when in default mode

--- tests/test_log_filters.py
+++ tests/test_log_filters.py
@@ -6,25 +6,30 @@ from requests_oauthlib.log_filters import DebugModeTokenFilter
 class TestDebugModeTokenFilter(unittest.TestCase):
 
     def setUp(self):
-        self.record = logging.LogRecord(name="test", level=logging.DEBUG, pathname=None, lineno=None, msg="Bearer i-am-a-little-token-here-is-my-scope-and-here-is-my-signature", args=None, exc_info=None)
+        self.record = logging.LogRecord(name="test", level=logging.DEBUG, pathname=None, lineno=None, msg="Bearer i-am-a-little-token-here-is-my-scope-and-here-is-my-hash", args=None, exc_info=None)
 
-    @patch.dict('os.environ', {'DEBUG_MODE_TOKEN_FILTER': 'MASK'})
+    @patch.dict('os.environ', {'REQUESTS_OAUTHLIB_DEBUG_MODE_TOKEN_FILTER': 'MASK'})
     def test_mask_mode(self):
         filter = DebugModeTokenFilter()
         filter.filter(self.record)
         self.assertIn('[MASKED]', self.record.msg)
 
-    @patch.dict('os.environ', {'DEBUG_MODE_TOKEN_FILTER': 'SUPPRESS'})
+    @patch.dict('os.environ', {'REQUESTS_OAUTHLIB_DEBUG_MODE_TOKEN_FILTER': 'SUPPRESS'})
     def test_suppress_mode(self):
         filter = DebugModeTokenFilter()
-        result = filter.filter(self.record)
-        self.assertFalse(result) # No logging
+        filter.filter(self.record)
+        self.assertEqual(" ", self.record.msg) # No logging
 
-    @patch.dict('os.environ', {'DEBUG_MODE_TOKEN_FILTER': 'DEFAULT'})
-    def test_default_mode_raises_warning(self):
-        filter = DebugModeTokenFilter()
-        result = filter.filter(self.record)
-        self.assertTrue(result)
+    # @patch.dict('os.environ', {'REQUESTS_OAUTHLIB_DEBUG_MODE_TOKEN_FILTER': 'DEFAULT'})
+    # def test_default_mode_raises_warning(self):
+    #     with self.assertLogs('requests_oauthlib', level='WARN') as cm:
+    #         DebugModeTokenFilter()
+    #         logging.getLogger("requests_oauthlib").addFilter(DebugModeTokenFilter())
+    #         # Trigger the log event to check for the warning message
+    #         logging.getLogger("requests_oauthlib").debug(self.record.getMessage())
+
+    #     self.assertIn("Your logger, when in DEBUG mode, will log TOKENS", cm.output[0])
+
 
 if __name__ == '__main__':
     unittest.main()
